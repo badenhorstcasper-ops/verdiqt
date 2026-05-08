ce1a62f0c2e114a6a2ac1121bbd6d76819bc0d82
badenhorstcasper-ops <badenhorst.casper@gmail.com>
2026-05-08 16:19:22 +0200

    kill sw

--- sw.js
+++ sw.js
@@ -1,22 +1,9 @@
-// Service worker v3 - minimal caching to allow auth redirects
-const CACHE = 'verdiqt-v9';
-const CONTENT_URL = 'https://raw.githubusercontent.com/badenhorstcasper-ops/verdiqt-content/main/verdiqt-content.json';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+// Service worker disabled - unregister and clear all caches
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
   );
-});
-
-// Don't intercept navigation requests - let auth redirects work
-self.addEventListener('fetch', e => {
-  if (e.request.mode === 'navigate') return;
-  const url = new URL(e.request.url);
-  if (url.hostname === 'api.anthropic.com') return;
 });
